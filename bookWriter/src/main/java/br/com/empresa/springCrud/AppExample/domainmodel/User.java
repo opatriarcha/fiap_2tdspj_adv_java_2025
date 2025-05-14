6ca370b5f81fc279f37759506074fb9930b3a1e3
Orlando C. Patriarcha <orlando.patriarcha@gmail.com>
2025-05-14 00:45:07 -0300

    add user details

--- bookWriter/src/main/java/br/com/empresa/springCrud/AppExample/domainmodel/User.java
+++ bookWriter/src/main/java/br/com/empresa/springCrud/AppExample/domainmodel/User.java
@@ -10,11 +10,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
-import java.util.Objects;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Entity
 @NoArgsConstructor
@@ -22,7 +23,7 @@ import java.util.UUID;
 @Table(name="USERS")
 @Cacheable
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     @Getter @Setter
@@ -68,6 +69,20 @@ public class User {
         User user = (User) o;
         return Objects.equals(id, user.id);
     }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.name;
+    }
+
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
 
     @Override
     public int hashCode() {
